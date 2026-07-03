6e7f87f80e6db34fe0afbb94b4b1d37d67e3bf2a
Swapnilrj-byte <swapyjaybhay2@gmail.com>
2026-07-03 15:38:45 +0530

    new

--- script.js
+++ script.js
@@ -6,5 +6,32 @@ function addToCart(item, price) {
         price: price
     });
 
+    updateCart();
+
     alert(item + " added to cart!");
+}
+
+function updateCart() {
+
+    let cartItems = document.getElementById("cart-items");
+    let total = document.getElementById("total");
+
+    cartItems.innerHTML = "";
+
+    let sum = 0;
+
+    cart.forEach(function(food) {
+
+        cartItems.innerHTML +=
+        "<p>" + food.item + " - ₹" + food.price + "</p>";
+
+        sum += food.price;
+
+    });
+
+    total.innerText = sum;
+
+    if (cart.length === 0) {
+        cartItems.innerHTML = "<p>Your cart is empty.</p>";
+    }
 }
